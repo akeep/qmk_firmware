c50c013715ce0c2fd0227048453bb01be924b07a
Andy Keep <andy.keep@gmail.com>
2023-09-16 11:10:03 -0700

    Updated layout for staggered DSP40.

--- keyboards/keebio/dsp40/keymaps/akeep-staggered/keymap.c
+++ keyboards/keebio/dsp40/keymaps/akeep-staggered/keymap.c
@@ -33,34 +33,56 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Default Layer
      * ,-------------------------------------------------------------.
-     * | Esc|  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P | BS   |
+     * | Esc | Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P | BS   |
      * |-------------------------------------------------------------|
      * | Tab |  A |  S |  D |  F |  G |  H |  J |  K |  L | Ent      |
      * |-------------------------------------------------------------|
      * | LSft   |  Z |  X |  C |  V |  B |  N |  M |  ,  |  . | RSft |
      * |-------------------------------------------------------------|
-     * | LCtl | LOpt | LGui | TO(_LOWER) |  spc   | TO(_ADJUST) | MO(_LOWER) | MO(_RAISE) |
-     * `-----------------------------------------------------------'
+     * | LCtl | LGui| LAlt|  spc fn0  |  spc fn1    |fn2|RAlt|RCtl   |
+     * `-------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT(
         KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,
         KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT,
-        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_RSFT,
-        KC_LCTL, KC_LOPT, KC_LGUI, TO(_LOWER), KC_SPC, TO(_ADJUST), MO(_LOWER), MO(_RAISE)
+        SC_LSPO, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  SC_RSPC,
+        KC_LCTL, KC_LGUI, KC_LOPT, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC), LT(_ADJUST, KC_ROPT), KC_RGUI, KC_RCTL
     ),
 
+    /* Lower Layer
+     * ,------------------------------------------------------------------------.
+     * |  ~  | !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |  BS   |
+     * |------------------------------------------------------------------------|
+     * | F1   | F2 | F3  | F4  | F5  | F5  |  F6  |  _ |  +  | {  | }  | Pipe   |
+     * |------------------------------------------------------------------------|
+     * | F7     | F8 | F9 | F10 | F11 | F12 | PIPE |  ±  |  : |   UP  |   ?     |
+     * |------------------------------------------------------------------------|
+     * |       |   |     |            |             |   LEFT   | DOWN | RIGHT   |
+     * `------------------------------------------------------------------------'
+     */
     [_LOWER] = LAYOUT(
-        KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_SCLN,  KC_LBRC, KC_RBRC, KC_BSLS, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_MINS, KC_EQL,   KC_SLSH, KC_QUOT, KC_UP,   KC_RSFT,
-        KC_LCTL, KC_LOPT, TO(_QWERTY), TO(_RAISE), KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),KC_DQUO, KC_COLN, KC_UP, KC_QUES,
+        _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
-    [_RAISE] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_BSPC,
-        KC_TAB,  KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19, KC_ENT,
-        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT,
-        _______, _______, _______, TO(_ADJUST), KC_SPC, TO(_LOWER), _______, RGB_TOG
+    /* Raise Layer
+     * ,------------------------------------------------------------------------.
+     * |  `  | 1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  BS   |
+     * |------------------------------------------------------------------------|
+     * | F1   |  F2 | F3  | F4  | F5  | F5  |  F6  |  - |  =  | [ | ] |   \     |
+     * |------------------------------------------------------------------------|
+     * | F7     | F8 | F9 | F10 | F11 | F12 |  \ |  §  |  ; |         |    /    |
+     * |------------------------------------------------------------------------|
+     * |       |   |     |            |             |          |      |         |
+     * `------------------------------------------------------------------------'
+     */
+     [_RAISE] = LAYOUT(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_QUOT, KC_SCLN, _______, KC_SLSH,
+        _______, KC_TRNS, _______, KC_TRNS, KC_TRNS, _______, _______, RGB_TOG
     ),
 
     [_ADJUST] = LAYOUT(
