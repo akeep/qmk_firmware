ce676ec64f7a9d261ef78d5bcf260c6ae330b96b
Andy Keep <andy.keep@gmail.com>
2024-05-04 19:33:20 -0700

    Small updates to Sinc keymap.

--- keyboards/keebio/sinc/keymaps/akeep/keymap.c
+++ keyboards/keebio/sinc/keymaps/akeep/keymap.c
@@ -14,7 +14,7 @@ void keyboard_post_init_user(void) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_80_with_macro(
-    KC_MUTE,             KC_ESC,  KC_BRMD, KC_BRMU, KC_MCTL, KC_LPAD, RGB_VAI, RGB_VAD, KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU, KC_SLEP, KC_PWR,
+    KC_MUTE,             KC_ESC,  KC_BRMD, KC_BRMU, KC_MCTL, KC_LPAD, RGB_VAI, RGB_VAD, KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU, KC_F13, KC_PWR,
     C(G(KC_Q)), KC_WAKE, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME,
     G(KC_X),    G(KC_C),  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
     G(KC_V),    A(S(G(KC_V))),  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef ENCODER_MAP_ENABLE
 // NOTE: My builds only have one encoder, but QMK seems to insist I list two.
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
-    [1] = { ENCODER_CCW_CW(KC_MNXT, KC_MPRV), ENCODER_CCW_CW(KC_MNXT, KC_MPRV) },
+    [0] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(KC_MNXT, KC_MPRV) },
+    [1] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(KC_MNXT, KC_MPRV) },
 };
 #endif
